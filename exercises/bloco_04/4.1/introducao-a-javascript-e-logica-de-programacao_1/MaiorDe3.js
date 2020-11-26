8751d384eb0381ccd2850c2c6b431a02deb781a6
Emanoel Leão <emanuelleaomarinho@gmail.com>
2020-11-25 22:13:29 -0300

    Adicionando programa que retonar o maior número de 3]

---
+++ exercises/bloco_04/4.1/introducao-a-javascript-e-logica-de-programacao_1/MaiorDe3.js
@@ -0,0 +1,11 @@
+let a = 11;
+let b = 5;
+let c = 20;
+
+if (a>b && a>c){
+    console.log("O número maior é o",a);
+} else if(b>a && b>c){
+    console.log("O número maior é o",b;
+} else {
+    console.log("O número maior é o",c);
+}
