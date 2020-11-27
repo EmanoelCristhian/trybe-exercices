3762c17e8c8f5083c0acd2e77c93d102651d813d
Emanoel Leão <emanuelleaomarinho@gmail.com>
2020-11-27 11:23:01 -0300

    Execício 02 resolvido

--- exercises/bloco_04/exe.01.js
+++ exercises/bloco_04/exe.01.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+
+/*
 //Exercício 01
 
 for(index = 0;index<numbers.length;index++){
@@ -7,3 +9,14 @@ for(index = 0;index<numbers.length;index++){
     console.log(numbers[index]);
 
 }
+*/
+
+
+//Exercício 02
+let aux = 0;
+
+for(index = 0;index<numbers.length;index++){
+    aux+=numbers[index];
+}
+
+console.log(aux);
